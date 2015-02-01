52703c61d7fbc59b3a0331f44711b2f903add439
Thomas Levine <_@thomaslevine.com>
2015-02-01 20:34:24 +0000

    simple query function

--- twistermap/ui.py
+++ twistermap/ui.py
@@ -1,6 +1,12 @@
+from random import choice
+
 from .network import build_network, query_network
 
-def q(thing, cache = {}):
+RANDOM = 1
+
+def q(thing = RANDOM, cache = {}):
     if 'network' not in cache:
         cache['network'] = build_network()
+    if thing == RANDOM:
+        thing = choice(list(cache['network'][1].keys()))
     return query_network(cache['network'], thing)
