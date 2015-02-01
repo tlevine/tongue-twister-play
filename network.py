bda1b33ded68d5f09ec3b5bd6e5d0379a0ab08b6
Thomas Levine <_@thomaslevine.com>
2015-02-01 17:33:01 +0000

    network.py

---
+++ network.py
@@ -0,0 +1,37 @@
+from collections import Counter
+import re
+
+from twister import twister
+import nltk
+
+def get_twisters():
+    stem = nltk.stem.PorterStemmer().stem
+    for language, original, translation in twister():
+        # Remove parantheses.
+        cleaned_translation = re.sub(r'\([^)]*\)', '', translation)
+        stems = list(map(stem, nltk.word_tokenize(cleaned_translation)))
+        yield language, original, translation, stems
+
+def main():
+    twisters = get_twisters()
+    scores = word_specialness_scores(twisters)
+
+def dependencies():
+    nltk.download('punkt')
+
+def word_specialness_scores(twisters):
+    'Determine how special each of the different word stems are.'
+    counts = Counter()
+    for _, _, _, stems in twisters:
+        counts.update(stems)
+        break
+    total_count = sum(counts.values())
+
+    return {k:v/total_count for k, v in counts.items()}
+
+
+
+
+#   scores[word]
+twisters = get_twisters()
+scores = word_specialness_scores(twisters)
