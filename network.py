531a4664e0200d2ac8b3bad4740d7e60a9bd2d8d
Thomas Levine <_@thomaslevine.com>
2015-02-01 18:21:58 +0000

    -

--- network.py
+++ network.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, defaultdict
 import re
 
 from twister import twister
@@ -10,12 +10,44 @@ def get_twisters():
         # Remove parantheses.
         if translation != None:
             cleaned_translation = re.sub(r'\([^)]*\)', '', translation)
-            stems = list(map(stem, nltk.word_tokenize(cleaned_translation)))
+            stopwords = nltk.corpus.stopwords.words()
+            stems = [stem(word.lower()) for word in nltk.word_tokenize(cleaned_translation) if word not in stopwords]
             yield language, original, translation, stems
 
-def main():
-    twisters = get_twisters()
+def build_network():
+    twisters = list(get_twisters())
+    linking_words = get_linking_words(twisters)
+
+    twisters_data = twisters_dict(twisters)
+    stem_twisters = defaultdict(set)
+    for _, original, _, stems in twisters:
+        for stem in stems:
+            if stem in linking_words:
+                stem_twisters[stem].add(original)
+                twisters_data[original]['linking_words'].add(stem)
+
+    network = stem_twisters, twisters_data
+    return network
+
+def query_network(network, twister_original):
+    stem_twisters, twisters_data = network
+    twisters_data[twister_original]['linking_words']
+
+def get_linking_words(twisters):
     counts = word_counts(twisters)
+    filtered_counts = {k:v for k,v in counts.items() if v > 1 and v < 110}
+    return set(filtered_counts.keys())
+
+def twisters_dict(twisters):
+    x = {}
+    for language, original, translation, stems in twisters:
+        x[original] = {
+            'language': language,
+            'original': original,
+            'translation': translation,
+            'linking_words': set(),
+        }
+    return x 
 
 def dependencies():
     nltk.download('punkt')
@@ -28,15 +60,7 @@ def word_counts(twisters):
         counts.update(stems)
     return counts
 
-def calibrate_scoring(counts):
+def sorted_counts(counts):
+    'Use this to calibrate the removal of junk.'
     remove_ones = filter(lambda pair: pair[1] > 1, counts.items())
     return sorted(remove_ones, key = lambda pair: pair[1])
-
-def count_twister(counts, twisters):
-    pass
-
-#   counts[word]
-twisters = get_twisters()
-counts = word_counts(twisters)
-sorted_counts = calibrate_scoring(counts)
-# nltk.corpus.stopwords.words
