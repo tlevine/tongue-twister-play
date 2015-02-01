0b87640c460c9d5a52afb3ead64d6b9ff0ade662
Thomas Levine <_@thomaslevine.com>
2015-02-01 20:35:31 +0000

    doc

--- twistermap/ui.py
+++ twistermap/ui.py
@@ -5,6 +5,11 @@ from .network import build_network, query_network
 RANDOM = 1
 
 def q(thing = RANDOM, cache = {}):
+    '''
+    Query for tongue-twisters related to ``thing``,
+    or do a ``RANDOM`` tongue-twister. Cache the network
+    after the first run.
+    '''
     if 'network' not in cache:
         cache['network'] = build_network()
     if thing == RANDOM:
