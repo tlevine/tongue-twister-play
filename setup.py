e1906ff2e4812308c1df73637950b6648b4c11fc
Thomas Levine <_@thomaslevine.com>
2015-02-05 04:58:19 +0000

    package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='tongue-twister-play',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Make a play of tongue twisters',
+      url='https://small.dada.pink/tongue-twister-play',
+      install_requires = ['vlermv','twister'],
+      packages=['twistermap'],
+      entry_points={'console_scripts': ['tongue-twister-play = twistermap:example']},
+      version='0.0.1',
+      license='AGPL',
+)
--- twistermap/__init__.py
+++ twistermap/__init__.py
@@ -1,1 +1,8 @@
 from .ui import q, play
+
+def example():
+    for tongue_twister in play():
+        args = (tongue_twister['translation'],
+                tongue_twister['language'],
+                tongue_twister['original'])
+        sys.stdout.write('%s\n(%s: %s)\n\n' % args)
